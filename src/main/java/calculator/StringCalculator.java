8ce9c9fcae395fba4ca68dda5f1761323bc27aca
Samadhan Sawant <samadhansawant15624@gmail.com>
2021-10-17 12:43:41 +0530

    code and testing for more digits to be supported

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -39,3 +39,11 @@ class StringCalculator {
     }
 
 }
+ private boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
